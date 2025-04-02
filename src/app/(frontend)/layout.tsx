a6112d84cfb659c7ca1440974332fa0ab9cace52
mohdshahzil <theshahzil@gmail.com>
2025-04-02 11:54:22 +0530

    somethingsss

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -11,7 +11,7 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
 
   return (
     <html lang="en">
-      <body>
+      <body className="bg-black text-white">
         <main>{children}</main>
       </body>
     </html>
